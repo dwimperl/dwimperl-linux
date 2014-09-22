c2914feace2517f140bd00e01a3123020bc43959
Gabor Szabo <gabor@szabgab.com>
2014-09-22 12:17:25 +0300

    split up the build to 3 steps

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/sh -e
 
+
 echo set up environment variables
 PERL_VERSION=5.20.1
 SUBVERSION=1
-ARCHITECTURE=`uname -i`
+PLATFORM=`uname`
+echo $PLATFORM
+if [ "$PLATFORM" == "Darwin" ]
+then
+  ARCHITECTURE=`uname -m`
+else
+  ARCHITECTURE=`uname -i`
+fi
+echo $ARCHITECTURE
+
 
 PERL_SOURCE_VERSION=perl-$PERL_VERSION
 PERL_SOURCE_ZIP_FILE=$PERL_SOURCE_VERSION.tar.gz
@@ -18,16 +28,32 @@ ORIGINAL_PATH=$PATH
 TEST_DIR=~/dwimperl_test
 BACKUP=~/dwimperl_backup
 
-tar xzf src/$PERL_SOURCE_ZIP_FILE
-cd $PERL_SOURCE_VERSION
-./Configure -des -Duserelocatableinc -Dprefix=$PREFIX_PERL
-make
-make test
-make install
-cd $BUILD_HOME
-
 export PATH=$PREFIX_PERL/bin:$ORIGINAL_PATH
-which perl
-perl -v
-prove
+
+case $1 in
+  perl)
+    echo "Building Perl"
+    tar xzf src/$PERL_SOURCE_ZIP_FILE
+    cd $PERL_SOURCE_VERSION
+    ./Configure -des -Duserelocatableinc -Dprefix=$PREFIX_PERL
+    make
+    make test
+    make install
+    cd $BUILD_HOME
+    
+    which perl
+    perl -v
+  ;;
+
+  modules)
+    cpanm --installdeps .
+  ;;
+  test)
+    prove
+  ;;
+
+  *)
+    echo "Missing or unrecognized parameter '$1'"
+  ;;
+esac
 
