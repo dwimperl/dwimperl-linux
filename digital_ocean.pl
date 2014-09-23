9922e5d5b194a7b6a3171be390a98537a7c97255
Gabor Szabo <gabor@szabgab.com>
2014-09-23 13:53:16 +0300

    working on the DO script

--- digital_ocean.pl
+++ digital_ocean.pl
@@ -1,64 +1,104 @@
 #!/usr/bin/perl
-use strict;
-use warnings;
 use 5.010;
+use Moo;
+use MooX::Options;
 
 use Config::Tiny;
 use Data::Dumper qw(Dumper);
 use DigitalOcean;
 use File::HomeDir;
 
+option list => (is => 'ro', doc => 'List available droplets');
+option verbose => (is => 'ro');
+option droplet => (is => 'rw', format => 's', doc => 'Droplet ID - Use this Droplet instead of creating new');
+option create => (is => 'ro');
+has config_file => (is => 'rw');
+has url => (is => 'rw');
 
-my $config_file = File::HomeDir->my_home . '/.digitalocean';
-my $url = 'https://github.com/dwimperl/dwimperl-linux/archive/experiment-0.01.zip';
-(my $zip_file = $url) =~ s{.*/}{};
-my $dir = 'dwimperl-linux-' . substr($zip_file, 0, -4);
-say $zip_file;
-say $dir;
+main->new_with_options->run;
 exit;
 
-main();
-exit;
+sub run {
+	my ($self) = @_;
 
-sub main {
-	usage('Missing config') if !-e $config_file;
+	$self->config_file(File::HomeDir->my_home . '/.digitalocean');
+	$self->url('https://github.com/dwimperl/dwimperl-linux/archive/experiment-0.01.zip');
+	(my $zip_file = $self->url) =~ s{.*/}{};
+	my $dir = 'dwimperl-linux-' . substr($zip_file, 0, -4);
+
+	$self->usage('Missing config') if !-e $self->config_file;
+
+	say $zip_file if $self->verbose;
+	say $dir if $self->verbose;
 	
-	my $Config = Config::Tiny->read( $config_file, 'utf8' );
+	my $Config = Config::Tiny->read( $self->config_file, 'utf8' );
 
 	my $ssh_key_id = delete $Config->{one}{ssh_key_id};
 	my $do = DigitalOcean->new(%{ $Config->{one} });
-	
-	say 'Creating droplet. Takes about 60 secons. Please wait';
-	my $t0 = time;
-	my $droplet = $do->create_droplet(
-	    name           => 'dwimperl',
-	    size_id        =>  66,        # 512Mb
-	    image_id       => '1601',     # CentOS 5.8 x64 
-	    ssh_key_ids    => $ssh_key_id,
-	    region_id      => 8,          # New York 3
-	    wait_on_event  => 1,
-	);
-	my $t1 = time;
-	say 'Elapsed time creating the Droplet: ' . $t1-$t0;
 
-	my $server = $do->droplet($droplet->id);
+	if ($self->list) {
+		for my $droplet (@{$do->droplets}) {
+			printf "Droplet %s has id %s and IP address %s\n", $droplet->name, $droplet->id, $droplet->ip_address;
+		}
+		return;
+	}
+
+
+	die "Exactly on of --create and   --droplet ID\n" if not $self->create xor $self->droplet;
+
+	
+	if ($self->create) {
+		say 'Creating droplet. Takes about 60 secons. Please wait';
+		my $t0 = time;
+		my $droplet = $do->create_droplet(
+		    name           => 'dwimperl',
+		    size_id        =>  66,        # 512Mb
+		    image_id       => '1601',     # CentOS 5.8 x64 
+		    ssh_key_ids    => $ssh_key_id,
+		    region_id      => 8,          # New York 3
+		    wait_on_event  => 1,
+		);
+		my $t1 = time;
+		say 'Elapsed time creating the Droplet: ' . $t1-$t0;
+		$self->droplet( $droplet->id );
+	}
+
+	my $server = $do->droplet($self->droplet);
 	printf "ID: %s  name %s IP: %s\n", $server->id, $server->name, $server->ip_address;
+
+	my $username = 'dwimperl';
+	#adduser $username 
+    #cp -r .ssh/ /home/$username/
+	#yum -y install make.x86_64
+	#yum -y install gcc.x86_64
 
 	my $cmd = sprintf 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@%s "uname -a; uptime; hostname"',  $server->ip_address;
 	say $cmd;
 	system $cmd;
 
-	say 'Destroying the server';
-	$server->destroy;
+    #ssh as $username
+    #  wget $url
+    #  unzip $zip_file
+    #  cd $dir
+    #  ./build.sh
+    #  cd ~
+    #  tar czf $zipname $dirname
+
+    #  dowload scp
+
+	if ($self->create) {
+		say 'Destroying the server';
+		$server->destroy;
+	}
 }
 
 sub usage {
-	my ($msg) = @_;
+	my ($self, $msg) = @_;
 	say $msg if $msg;
 
+	my $config_file = $self->config_file;
 	print <<"END_USAGE";
 Usage: $0
-
 
 In order to access the DigitaOcean API we need the API keys in $config_file.
 [one]
