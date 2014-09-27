628396a34a76aada31ef0026dc4a8298c59e9a08
Gabor Szabo <gabor@szabgab.com>
2014-09-27 22:30:29 +0300

    oversight?

--- dwim.sh
+++ dwim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # The directory of this script
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PERL5OPT="-MDWIM"
 
