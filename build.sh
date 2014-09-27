490a8e3cae2be1cbdb40619885cc33793b93dffe
Gabor Szabo <gabor@szabgab.com>
2014-09-27 23:33:36 +0300

    --notest Class::Singleton; no DateTime

--- build.sh
+++ build.sh
@@ -199,6 +199,8 @@ case $1 in
       source $ROOT/dwim.sh
       $PREFIX_PERL/bin/perl $PREFIX_PERL/bin/cpanm --mirror file://$SOURCE_HOME/local/cache/ --mirror-only --notest IPC::System::Simple
       # see https://travis-ci.org/dwimperl/dwimperl-linux/builds/36451393
+
+      $PREFIX_PERL/bin/perl $PREFIX_PERL/bin/cpanm --mirror file://$SOURCE_HOME/local/cache/ --mirror-only --notest Class::Singleton
   ;;
 
   try)
@@ -209,7 +211,6 @@ case $1 in
       #ls -l $ROOT
       source $ROOT/dwim.sh
       #$PREFIX_PERL/bin/perl $PREFIX_PERL/bin/cpanm --mirror file://$SOURCE_HOME/local/cache/ --mirror-only --verbose IPC::System::Simple
-      #$PREFIX_PERL/bin/perl $PREFIX_PERL/bin/cpanm --mirror file://$SOURCE_HOME/local/cache/ --mirror-only --verbose Class::Singleton
       #$PREFIX_PERL/bin/perl $PREFIX_PERL/bin/cpanm --mirror file://$SOURCE_HOME/local/cache/ --mirror-only --verbose DateTime
       #$PREFIX_PERL/bin/perl $PREFIX_PERL/bin/cpanm --mirror file://$SOURCE_HOME/local/cache/ --mirror-only --verbose MongoDB
   ;;
